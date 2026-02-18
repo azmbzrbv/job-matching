e69bf691bc06b8cf8fd133805e5d950df98c9634
Azim Maksatbek uulu <bzrbvazm@gmail.com>
2026-02-18 13:15:01 +0100

    Backend skeleton

---
+++ backend-java/src/main/java/com/jobmatching/JobMatchingApplication.java
@@ -0,0 +1,13 @@
+package com.jobmatching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobMatchingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobMatchingApplication.class, args);
+	}
+
+}
---
+++ backend-java/src/test/java/com/jobmatching/JobMatchingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobmatching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobMatchingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
